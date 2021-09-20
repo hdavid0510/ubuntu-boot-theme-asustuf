12453ae203c3b8f6e080277997724fa038c8c98b
David Hong <gdavid0510@gmail.com>
2021-09-20 15:45:18 +0900

    bugfix

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo cp -r grub-theme/asustuf /boot/grub/themes/
 sudo sed -i '/GRUB_THEME=/d' /etc/default/grub
 echo -e "\nGRUB_THEME=asustuf" | sudo tee -a /etc/default/grub
-update-grub
+sudo update-grub
 
 # Plymouth theme install
 sudo cp -r plymouth-theme/asustuf /usr/share/plymouth/themes/
