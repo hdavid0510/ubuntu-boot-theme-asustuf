55cacd15aa9c0390c942bddd80aa65ad16d3ce10
David Hong <gdavid0510@gmail.com>
2021-09-20 15:23:47 +0900

    bugfix

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ update-grub
 
 # Plymouth theme install
 sudo cp -r plymouth-theme/asustuf /usr/share/plymouth/themes/
-sudo update-alternative --install default.plymouth /usr/share/plymouth/themes/asustuf/asustuf.plymouth 200
-sudo update-alternative --auto default.plymouth
+sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/asustuf/asustuf.plymouth 200
+sudo update-alternatives --auto default.plymouth
